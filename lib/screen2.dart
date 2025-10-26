d105f09d7a550c9ae47f679dd8dae447d938de2c
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-10-26 22:17:12 +0600

    drawer widget added

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -6,6 +6,29 @@ class Screen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              margin: EdgeInsets.zero,
+              child: Column(
+               children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(500),
+                  child: Image.network("https://picsum.photos/200", height: 100, errorBuilder: (_,__,___){
+                    return Icon(Icons.image, size: 50,);
+                  },),
+                ),
+                Text("Random Image Profile")
+              ],
+            ))
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
        body: Column(
         mainAxisAlignment:MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
